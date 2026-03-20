00ea1126de30252d1d8babc10bd7d1ef7c357299
Ishrat Joyeeta <25ishratjoyeeta@gmail.com>
2026-03-20 14:31:36 +0000

    Add files via upload

---
+++ Python_programming_practice/Fundamentals/basic_function.py
@@ -0,0 +1,8 @@
+"""Write a program to create a function that takes
+two arguments, name and age, and prints their values."""
+
+def function(name, age):
+    print(name)
+    print(age)
+    
+test = function("Tyla", 16)
